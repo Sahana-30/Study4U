6d6952323943120e1037407edc3ef5460fa48c58
SahanaGokulakri <>
2021-05-16 12:32:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+<button id="top" style="background-color:transparent; border:2px solid transparent; margin-left:1300px; margin-top:120px;"> <img id="arrow" style="width:40px;height:60px;" src="images/arrow.png"><br> </button> 
+
+<script> 
+var top = document.getElementById('top');
+
+function up(){
+  window.scrollTo(0,0);
+}
+
+top.addEventListener("click",up);
+</script> 
+
+
+
